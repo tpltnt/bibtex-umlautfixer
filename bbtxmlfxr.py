4055c2d1cb122b7881b373f7df1eb29866955c1a
tpltnt <tpltnt.github@dropcut.net>
2014-06-29 19:32:44 +0200

    first shot

--- bbtxmlfxr.py
+++ bbtxmlfxr.py
@@ -8,3 +8,22 @@ if 2 != len(sys.argv):
     print("you did something wrong")
     sys.exit(1)
 
+with open(sys.argv[1], 'r') as bibtexfile:
+    with open("tmp.bibtex", 'w') as newfile:
+        ignore = False
+        for line in bibtexfile:
+            # ignore abstract and review field
+            if 'abstract = {' in line:
+                ignore = True
+            if 'review = {' in line:
+                ignore = True
+            if '}' in line:
+                ignore = False
+            # replacement action
+            if not ignore:
+                line = line.replace('ä', '{\"a}')
+                line = line.replace('ö', '{\"o}')
+                line = line.replace('ü', '{\"u}')
+                line = line.replace('ß', '{\ss}')
+            newfile.write(line)
+
